38d150d72e0a4c30963871b0b7ce09ef6063b414
emet67 <ericrene.mettler@student.unisg.ch>
2025-12-11 18:33:58 +0100

    Update ahah.py

--- ahah.py
+++ ahah.py
@@ -647,8 +647,6 @@ if st.session_state.step >= 4 and st.session_state.evaluation_done:
             st.success(msg)
             st.session_state.final_success_message = False
 
-        })
-
         df_final = s_t[s_t["track_id"].isin(st.session_state.recommended_ids)][["title", "artist"]]
         df_final_display = df_final.reset_index(drop=True)
         df_final_display.index = df_final_display.index + 1
