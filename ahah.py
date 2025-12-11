d5b3c3b9b0554aa2523634470c1c487f9a4d1e58
emet67 <ericrene.mettler@student.unisg.ch>
2025-12-11 19:34:06 +0100

    Update ahah.py

--- ahah.py
+++ ahah.py
@@ -16,12 +16,8 @@ from ast import literal_eval
 # ---------------------------------------------
 
 def get_conn():                                                     # Define get function to connect with sqlite3
-    try:
-        conn = sqlite3.connect("dfdf")
-        return conn
-    except sqlite3.Error:
-        st.error("Database connection failed! Please check, that you downloaded it and left it in the folder 'data'")
-        st.stop
+    return sqlite3.connect("data/app.db")
+       
 DB = get_conn()                                                     # assign the database to the variable DB
 
 st.set_page_config(
